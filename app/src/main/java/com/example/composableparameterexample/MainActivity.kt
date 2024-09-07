8f2d05a91406edbc94fd0fad409061f2e2de12b8
andersbor <anbo@zealand.dk>
2024-09-07 21:17:27 +0200

    Stupid top app bar

--- app/src/main/java/com/example/composableparameterexample/MainActivity.kt
+++ app/src/main/java/com/example/composableparameterexample/MainActivity.kt
@@ -7,11 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composableparameterexample.ui.theme.ComposableParameterExampleTheme
@@ -22,7 +28,31 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposableParameterExampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { // Don't do this at home
+                        Button(onClick = {}) { Text(text = "Top bar button") }
+                    },
+                    floatingActionButton = { // Don't do this at home
+                        Text(text = "Floating action button")
+                    }
+
+                ) { innerPadding ->
+                    Column(modifier = Modifier.padding(innerPadding)) { // Column is a composable that places its children in a vertical sequence
+                        var switchState by remember { mutableStateOf(true) }
+                        LabeledComposable(text = "On or Off") {
+                            Switch(checked = switchState, onCheckedChange = { switchState = it })
+                        }
+
+                        var sliderValue by remember { mutableFloatStateOf(0f) }
+                        LabeledComposable(text = "Rating") {
+                            Slider(
+                                value = sliderValue, onValueChange = { sliderValue = it },
+                                valueRange = 1f..10f, steps = 8
+                            )
+                        }
+                        Text(text = "Slider value: $sliderValue")
+                    }
                     LabeledComposable(
                         modifier = Modifier.padding(innerPadding),
                         text = "Rating"
@@ -39,20 +69,12 @@ class MainActivity : ComponentActivity() {
 fun LabeledComposable(
     text: String,
     modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
+    content: @Composable (name: String) -> Unit
 ) {
     Column(modifier = modifier) {
         Text(text = text)
-        content()
+        content(name = "Anders")
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
